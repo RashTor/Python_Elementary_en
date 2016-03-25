545f188b55f42e036f61d22c5bb01b11cedc5d68
RashTor <v1992h@mail.ru>
2016-03-25 15:18:50 +0200

    prog_edit

--- 2_2.py
+++ 2_2.py
@@ -1,4 +1,21 @@
-import random, time
+import time,random
+
+
+class profile:
+    def __init__(self):
+        self.num_of_calls = 0
+        self.name_of_func_s = []
+
+    def __call__(self, fun_name):
+        self.num_of_calls+=1
+        self.name_of_func_s.append(fun_name.__name__)
+        print self.num_of_calls
+        print self.name_of_func_s
+        def decor_comp_time(fun_arg):
+             beg_time=time.clock()
+             fun_name(fun_arg)
+             print time.clock()-beg_time
+        return decor_comp_time
 
 
 def bubble_sort(elem_range):
@@ -17,18 +34,15 @@ def generate_range(max_numb):
     return rand_range
 
 
-def comp_time_of_func(fun_to_decor):
-    def decor_comp_time(rand_range):
-        beg_time=time.clock()
-        fun_to_decor(rand_range)
-        print time.clock()-beg_time
-    return decor_comp_time
-
-
 def main():
+    obj_class_profile=profile()
     #unsort_list=generate_range(input("Input max number of elements:"))
-    decor_bubble_sort=comp_time_of_func(bubble_sort)
+    decor_bubble_sort=obj_class_profile(bubble_sort)
+    decor_gen_range=obj_class_profile(generate_range)
+    #print type (generate_range(input("Input max number of elements:")))
     decor_bubble_sort(generate_range(input("Input max number of elements:")))
+    decor_gen_range(8)
+
 
 if __name__ == '__main__':
     main()
